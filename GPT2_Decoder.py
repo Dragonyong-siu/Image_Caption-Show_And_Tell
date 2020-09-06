ecd38e4aca106738d95a984fa7b39a89a6788662
Dragonyong-siu <68500343+Dragonyong-siu@users.noreply.github.com>
2020-09-07 02:31:07 +0900

    Update GPT2_Decoder.py

--- GPT2_Decoder.py
+++ GPT2_Decoder.py
@@ -5,15 +5,15 @@ class Image_Caption_Model_GPT2(nn.Module):
     self.GPT2_Model = GPT2_Model
     self.GPT2_wte = self.GPT2_Model.wte
     self.GPT2_wpe = self.GPT2_Model.wpe
-    ## self.GPT2_drop = self.GPT2_Model.drop
+    self.GPT2_drop = self.GPT2_Model.drop
     self.GPT2_h = self.GPT2_Model.h
     self.GPT2_ln_f = self.GPT2_Model.ln_f
     self.GPT2_Hidden = 768
-    self.GPT2_layers = 6
+    self.GPT2_layers = 12
     self.Linear_FC = nn.Linear(8 * 8 * 512, self.GPT2_Hidden)
     self.Linear_LM = nn.Linear(self.GPT2_Hidden, 50260)
     self.ReLU = nn.ReLU(inplace = True)
-    self.Dropout = nn.Dropout(0.01, inplace = True)
+    self.Dropout = nn.Dropout(0.3, inplace = True)
 
   def forward(self, input_ids, position_ids, feature_map):
     feature_map = feature_map.view(-1, 8 * 8 * 512)
@@ -24,12 +24,12 @@ class Image_Caption_Model_GPT2(nn.Module):
     position_wpe = self.GPT2_wpe(position_ids) 
     
     input_Embedding = input_Embedding + position_wpe
-    ## input_Embedding = self.GPT2_drop(input_Embedding)
+    input_Embedding = self.GPT2_drop(input_Embedding)
     input_Embedding = input_Embedding.to(device) 
     
     for i in range(self.GPT2_layers):
       input_Embedding = self.GPT2_h[i](input_Embedding)[0]
-    
+
     Hidden_states = self.GPT2_ln_f(input_Embedding)
     Logits = self.Linear_LM(Hidden_states)
     Logits = self.Dropout(Logits)
@@ -37,29 +37,33 @@ class Image_Caption_Model_GPT2(nn.Module):
   
   def Image_Caption_Sampling(self, feature_map, max_len):
     feature_map = feature_map.view(-1, 8 * 8 * 512)
-    Sampling_inputs = self.Linear_FC(feature_map)
+    feature_inputs = self.Linear_FC(feature_map)
+    feature_inputs = feature_inputs.unsqueeze(1)
 
     Sample_Ids = []
-    Position_inputs = self.GPT2_wpe(torch.Tensor([0]).to(device).long())
     for i in range(max_len):
-      Sampling_inputs = Sampling_inputs.unsqueeze(1)
-      Sampling_inputs = Sampling_inputs.to(device) 
+      Addition_inputs = torch.Tensor(Sample_Ids)
+      Addition_inputs = Addition_inputs.long().to(device)
+      Addition_inputs = self.GPT2_wte(Addition_inputs)
+      Addition_inputs = Addition_inputs.unsqueeze(0)
+     
+      Sampling_inputs = torch.cat((feature_inputs, Addition_inputs), dim = 1)
+      inputs_length = Sampling_inputs.size(1)
+      Position_inputs = torch.Tensor(np.arange(inputs_length))
+      Position_inputs = Position_inputs.long().to(device)
+      Position_inputs = self.GPT2_wpe(Position_inputs)
       
-      Sampling_inputs = Sampling_inputs + Position_inputs
+      Model_inputs = Sampling_inputs + Position_inputs
+      Model_inputs = Model_inputs.to(device) 
+      Sampling_inputs = self.GPT2_drop(Sampling_inputs)
       for j in range(self.GPT2_layers):
-        Sampling_inputs = self.GPT2_h[j](Sampling_inputs)[0]
+        Model_inputs = self.GPT2_h[j](Model_inputs)[0] 
       
-      hidden_states = self.GPT2_ln_f(Sampling_inputs)
-      Sampling_outputs = self.Linear_LM(hidden_states.squeeze(1))
-      Words_Index = Next_Word_Index(Sampling_outputs)
+      hidden_states = self.GPT2_ln_f(Model_inputs)
+      Sampling_outputs = self.Linear_LM(hidden_states)
+      Words_Index = Next_Word_Index(Sampling_outputs[:, -1, :])
       Words_Index = torch.Tensor([Words_Index]).long().to(device)
       Sample_Ids.append(Words_Index) 
-      
-      Sampling_inputs = self.GPT2_wte(Words_Index) 
-      Position_inputs = self.GPT2_wpe(torch.Tensor([i + 1]).to(device).long())
-      
-      Sampling_inputs = Sampling_inputs + Position_inputs
-      Sampling_inputs = Sampling_inputs.to(device)
     Sample_Ids = torch.stack(Sample_Ids, dim = 1)
     return Sample_Ids
   
@@ -86,7 +90,7 @@ def Next_Word_Index(logits):
 
   Index_MAZINO = []
   for i in range(len(Index_List)):
-    if Index_List[i][1] >= 0.3:
+    if Index_List[i][1] >= 0.35:
       Index_MAZINO.append(Index_List[i][0])
   
   if len(Index_MAZINO) == 0:
