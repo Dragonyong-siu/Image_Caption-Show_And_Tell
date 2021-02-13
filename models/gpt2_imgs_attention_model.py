4a9136cf53587df6d8fdaa117df82e96d98ae10e
Dragonyong-siu <68500343+Dragonyong-siu@users.noreply.github.com>
2021-02-14 01:47:20 +0900

    Update gpt2_imgs_attention_model.py

--- models/gpt2_imgs_attention_model.py
+++ models/gpt2_imgs_attention_model.py
@@ -1,9 +1,11 @@
-# model : encoder-resnet, decoder-transformer
+# model3 : encoder-vgg, decoder-transformer
 
 import torch.nn as nn
 from pytorchcv.model_provider import get_model as ptcv_get_model
+from torch.nn.utils.weight_norm import weight_norm
 
-cnn = ptcv_get_model('vgg19', pretrained = True) # not trainable parameters
+device = 'cuda'
+cnn = ptcv_get_model('vgg19', pretrained = True)#'efficientnet_b7c', pretrained = True) # not trainable parameters
 
 class decoder(nn.Module):
   def __init__(self, features_dim, embed_dim, seq_len = hyper_parameters['max_len'], dropout = 0.5):
@@ -20,7 +22,7 @@ class decoder(nn.Module):
     self.__img2embed_layer__ = nn.Linear(self.features_dim, self.embed_dim)
     self.__content_embed__ = gpt2_model.wte
     self.__position_embed__ = gpt2_model.wpe
-    self.__embed_drop__ = gpt2_model.drop
+    self.__embed_drop__ = nn.Dropout(p = 0.5)
     self.__hidden_layers__ = gpt2_model.h
     self.__layer_norm__ = gpt2_model.ln_f
     self.__fc_layer__ = nn.Linear(self.embed_dim, self.vocab_dim)
@@ -31,15 +33,16 @@ class decoder(nn.Module):
     self.__init_weights__()
 
   def __init_weights__(self):
-    self.__fc_layer__.bias.data.fill_(0)
-    self.__fc_layer__.weight.data.uniform_(-0.1, 0.1)
+    ## self.__fc_layer__.bias.data.fill_(0)
+    ## self.__fc_layer__.weight.data.uniform_(-0.1, 0.1)
+    torch.nn.init.xavier_uniform_(self.__fc_layer__.weight)
 
   def __random_topk__(self, pred, k): 
     prob_distribution = self.softmax(pred)
     top_indices = prob_distribution.topk(k = k).indices.permute(1, 0)
 
     return random.choice(top_indices)
-  
+
   def __img_block__(self, images, block_num):
     batch, c, h, w = images.shape
 
@@ -52,47 +55,54 @@ class decoder(nn.Module):
   def forward(self, images, input_ids = None):
     batch = images.shape[0] # (N)
 
-    image_blocks = self.__img_block__(images, self.block_num).reshape(-1, 3, self.block_size, self.block_size)
-    batch_features = self.__encoder__(image_blocks).reshape(batch, int(self.block_num ** 2), self.features_dim) 
+    img_blocks = self.__img_block__(images, self.block_num).reshape(-1, 3, self.block_size, self.block_size)
+    with torch.no_grad():
+      batch_features = self.__encoder__(img_blocks).reshape(batch, int(self.block_num ** 2), self.features_dim) 
     imgs_embed = self.__img2embed_layer__(batch_features) # (N, block_num ** 2, embed_dim)
 
-    words_embed = self.__content_embed__(input_ids[:, 1:])
-    indices = torch.arange(self.seq_len + int(self.block_num ** 2) - 1).expand(batch, -1).to(device)
+    words_embed = self.__content_embed__(input_ids)
+    indices  = torch.arange(self.seq_len + int(self.block_num ** 2)).expand(batch, -1).to(device)
     position_embed = self.__position_embed__(indices)
 
     h = self.__embed_drop__(torch.cat([imgs_embed, words_embed], dim = 1) + position_embed).to(device) # (N, seq_len, embed_dim)
     for i in range(self.layer_num):
       h = self.__hidden_layers__[i](h)[0]
-  
-    preds = self.__fc_layer__(self.dropout(self.__layer_norm__(h))) # (N, seg_len + block_num ** 2 - 1, vocab_dim)
-    return preds[:, (int(self.block_num ** 2) - 1):, :]
+      h[:, :(self.block_num ** 2), :] = imgs_embed + position_embed[:, :(self.block_num ** 2), :]
+
+    preds = self.__fc_layer__(self.dropout(self.__layer_norm__(h))) # (N, seg_len + block_num ** 2, vocab_dim)
+    return preds[:, int(self.block_num ** 2):, :]
   
   def __sample__(self, images):
     batch = images.shape[0] # (N)
-
-    image_blocks = self.__img_block__(images, self.block_num).reshape(-1, 3, self.block_size, self.block_size)
-    batch_features = self.__encoder__(image_blocks).reshape(batch, int(self.block_num ** 2), self.features_dim)
+  
+    img_blocks = self.__img_block__(images, self.block_num).reshape(-1, 3, self.block_size, self.block_size)
+    batch_features = self.__encoder__(img_blocks).reshape(batch, int(self.block_num ** 2), self.features_dim)
     imgs_embed = self.__img2embed_layer__(batch_features) # (N, block_num ** 2, embed_dim)
      
-    h = imgs_embed + self.__position_embed__(torch.arange(4).expand(batch, -1)).to(device).long()) # (N, block_num ** 2, embed_dim)
+    start_embed = self.__content_embed__(torch.Tensor(batch * [50258]).to(device).long())  
+    indices = torch.arange(int(self.block_num ** 2) + 1).expand(batch, -1).to(device).long()
+    position_embed = self.__position_embed__(indices)
+
+    h = (torch.cat([imgs_embed, start_embed.unsqueeze(1)], dim = 1) + position_embed).to(device) # (N, block_num ** 2 + 1, embed_dim)
 
     preds = torch.zeros([batch, self.seq_len]).to(device)
     scores = torch.zeros([batch, self.seq_len, self.vocab_dim]).to(device)
     for i in range(self.seq_len):
       for j in range(self.layer_num):
         h = self.__hidden_layers__[j](h)[0]
+        h[:, :(self.block_num ** 2), :] = imgs_embed + position_embed[:, :(self.block_num ** 2), :]
 
       pred = self.__fc_layer__(self.__layer_norm__(h))[:, -1, :] # (N, vocab_dim)
       preds[:, i] = self.__random_topk__(pred = pred, k = 1) 
       scores[:, i, :] = pred
       
       words_embed = self.__content_embed__(preds[:, :(i + 1)].long())
-      indices = torch.arange(i + 5).expand(batch, -1).to(device)
+      indices = torch.arange(i + int(self.block_num ** 2) + 2).expand(batch, -1).to(device)
       position_embed = self.__position_embed__(indices)
 
-      h = torch.cat([imgs_embed, words_embed], dim = 1) + position_embed
+      h = torch.cat([imgs_embed, start_embed.unsqueeze(1), words_embed], dim = 1) + position_embed
 
     return scores
 
-gpt_decoder = decoder(features_dim = 512 * 3 * 3,
+gpt_decoder = decoder(features_dim = 512 * 2 * 2,#2560,
                       embed_dim = 768)
